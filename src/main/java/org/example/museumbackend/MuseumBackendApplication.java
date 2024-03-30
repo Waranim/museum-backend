8e239b2273cec639bacd8329f3401d02fae91f1a
Waranim <129619485+Waranim@users.noreply.github.com>
2024-03-30 14:49:22 +0500

    Init commit

---
+++ src/main/java/org/example/museumbackend/MuseumBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.museumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseumBackendApplication.class, args);
+    }
+
+}
